f9ed5407a373014afdacf6720b3f3922b6e29326
Bowen Zhang <zbw1987713@163.com>
2012-10-01 00:27:41 -0400

    kmeans finished and hospital location found

--- hw3/patient.cpp
+++ hw3/patient.cpp
@@ -4,7 +4,7 @@ using namespace std;
 Patient::Patient(int x,int y,int t,int i): 
   Position(x,y),
   resecue_time_(t),
-  id(i){
+  id(i) {
 }
 
 void Patient::output() {
@@ -16,11 +16,15 @@ bool Patient::isPatient() {
 bool Hospital::isPatient() {
   return false;
 }
-Hospital::Hospital(int x,int y,int i):
+Hospital::Hospital(int x,int y,int i,int ab):
   Position(x,y),
-  id(i){
+  id(i),
+  ambulance_number_(ab){
 }
 
 void Hospital::output() {
   cout<<"H"<<id<<','<<x<<','<<y;
+}
+void Hospital::outputHos() {
+  cout<<"H"<<id<<','<<x<<','<<y<<','<<ambulance_number_<<endl;
 }
--- hw3/patient.h
+++ hw3/patient.h
@@ -1,19 +1,26 @@
 #ifndef patient_h_
 #define patient_h_
+#include <cstdlib>
 class Position {
  public:
   int x;
   int y;
+  Position() {};
  Position(int xx,int yy): x(xx),y(yy){};
-  virtual void  output() = 0;
-  virtual bool isPatient() = 0;
+  virtual void output() {};
+  virtual bool isPatient(){return false;};
+  int distance(Position* p) {
+    return abs(p->x-x)+ abs(p->y-y);
+  };
 };
 class Hospital:public Position {
  private:
   int id;
+  int ambulance_number_;
  public:
-  Hospital(int x,int y,int id);
+  Hospital(int x,int y,int id,int an);
   void output();
+  void outputHos();
   bool isPatient();
 };
 class Patient:public Position  {
