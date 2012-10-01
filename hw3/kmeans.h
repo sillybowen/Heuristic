14b02883afb10d97cc821acc0be4cd82b3eaa8d8
Bowen Zhang <zbw1987713@163.com>
2012-10-01 00:49:48 -0400

    ambulance initial state setup

---
+++ hw3/kmeans.h
@@ -0,0 +1,6 @@
+#ifndef kmeans_h_
+#define kmeans_h_
+void kmeansToFindHospital(vector<Patient*> &patients,
+                          vector<int> &ambNumber,
+                          vector<Hospital*> &hospitals);
+#endif
